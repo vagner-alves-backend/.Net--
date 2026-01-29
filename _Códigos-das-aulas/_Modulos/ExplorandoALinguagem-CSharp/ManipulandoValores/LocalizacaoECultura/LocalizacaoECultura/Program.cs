7d47513d53c09cadc03b319ddce09bed91fca548
vagner-alves-backend <vagner.alves.dev@gmail.com>
2026-01-28 22:11:45 -0300

    --Alterando a localização do seu sistema.

---
+++ _Códigos-das-aulas/_Modulos/ExplorandoALinguagem-CSharp/ManipulandoValores/LocalizacaoECultura/LocalizacaoECultura/Program.cs
@@ -0,0 +1,10 @@
+﻿//Normalmente a localização é definida de forma altomatica, usando as configurações do sistema, mas 
+//para forçar essa mudança podemos usar o comando.:
+using System.Globalization;
+decimal valorMonetario = 1245.45M;
+Console.WriteLine($"Por padrão do meu sistema..: {valorMonetario, 10:C}");
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+Console.WriteLine($"Sistema en-US..: {valorMonetario, 10:C}");
+
+//Alterando a localização da cultura.:
+Console.WriteLine($"Sistema es-ES..: {valorMonetario.ToString("C", new CultureInfo("pt-BR"))}");
