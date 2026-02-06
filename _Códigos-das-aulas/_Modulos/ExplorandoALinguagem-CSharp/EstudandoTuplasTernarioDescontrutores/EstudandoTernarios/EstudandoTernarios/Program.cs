2997612861f4a9557d5875445d588654bde6d556
vagner-alves-backend <vagner.alves.dev@gmail.com>
2026-02-06 16:48:05 -0300

    --Testando a estrutura de IF TERNARIO.

---
+++ _Códigos-das-aulas/_Modulos/ExplorandoALinguagem-CSharp/EstudandoTuplasTernarioDescontrutores/EstudandoTernarios/EstudandoTernarios/Program.cs
@@ -0,0 +1,22 @@
+﻿bool _continue = false;
+string? numberText = "";
+int number = 0;
+int resultado = 0;
+
+Console.Clear();
+do
+{
+    Console.Write("Informe um number par..: ");
+    numberText = Console.ReadLine();
+    Console.Clear();
+    while (!int.TryParse(numberText, out _))
+    {
+        Console.Write("O valor informado não é um number,\nFavor informe um numbe -> ");
+        numberText = Console.ReadLine();
+    }
+    number = Convert.ToInt32(numberText);
+    resultado = number % 2;
+
+    Console.WriteLine($"O número {number} é {(resultado == 0 ? "par" : "Impar")}");
+    _continue = number % 2 == 1;
+} while (_continue);
