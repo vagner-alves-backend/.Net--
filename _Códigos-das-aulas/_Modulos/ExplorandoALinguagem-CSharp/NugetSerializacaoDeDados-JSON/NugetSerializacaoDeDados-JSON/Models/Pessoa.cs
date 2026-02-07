3abec93fdd9b0fe9da93163ae28375ec1d16547a
vagner-alves-backend <vagner.alves.dev@gmail.com>
2026-02-06 22:13:10 -0300

    --Estudando Nuget e Serialização com arquivos .json, inicio do modulo de (nuget, serialização e atributos) da dio.me

---
+++ _Códigos-das-aulas/_Modulos/ExplorandoALinguagem-CSharp/NugetSerializacaoDeDados-JSON/NugetSerializacaoDeDados-JSON/Models/Pessoa.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NugetSerializacaoDeDados_JSON.Models
+{
+    public class Pessoa(string? name, int idade, string? dataNacimento)
+    {
+        public string? Name = name;
+        public int Idade = idade;
+        public string? DataNascimento = dataNacimento;
+    }
+}
---
+++ _Códigos-das-aulas/_Modulos/ExplorandoALinguagem-CSharp/NugetSerializacaoDeDados-JSON/NugetSerializacaoDeDados-JSON/Program.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using NugetSerializacaoDeDados_JSON.Models;
+
+string? caminho = "C:\\Users\\Vágner Alves\\OneDrive\\Documentos\\DioMe-Curso-CSharp\\_Códigos-das-aulas\\_Modulos\\ExplorandoALinguagem-CSharp\\NugetSerializacaoDeDados-JSON\\NugetSerializacaoDeDados-JSON\\Arquivos\\arquivo.json";
+
+Console.Clear();
+Pessoa pessoa = new("Vágner Alves", 20, "06/03/2005");
+string? serializacao = JsonConvert.SerializeObject(pessoa, Formatting.Indented);
+Console.WriteLine(serializacao);
+
+File.WriteAllText(caminho, serializacao);
