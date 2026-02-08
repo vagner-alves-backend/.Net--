8a310aed2bc8d2123f02e7e2d11a6b4d302a2cef
vagner-alves-backend <vagner.alves.dev@gmail.com>
2026-02-07 23:41:20 -0300

    --Inicianto estudos de tipos nulos.

---
+++ _Códigos-das-aulas/_Modulos/ExplorandoALinguagem-CSharp/ValoresNulosEAnonimos/ValoresNulosEAnonimos/Program.cs
@@ -0,0 +1,12 @@
+﻿bool? isNull = null;
+//isNull.HasValue == Nesse cenario se o valor for null, ele irá retornar false.
+
+// if (!isNull.HasValue)
+// {
+//     Console.WriteLine("Value null.");
+// } else
+// {
+//     Console.WriteLine($"Value is {isNull}.");
+// }
+
+Console.WriteLine($"O valor é {(isNull.HasValue ? isNull: "Null")}.");
